258e541772050a1379463b32f1edad4a3564b085
trevorwhitaker <trevor.whitaker@hotmail.com>
2017-01-06 20:57:38 -0500

    fixed bug

--- resources/views/Admin/index.blade.php
+++ resources/views/Admin/index.blade.php
@@ -4,25 +4,8 @@
 
 @section('title', ' | Post')
 
-<a href="{{ route('admin.getAuthors') }}" class="btn btn-info" style="margin-top: 15px;">Manage Authors</a>
+<a href="{{ route('admin.getAuthors') }}" class="btn btn-info">Manage Authors</a>
 
-<table class="table table-hover" style="margin-top: 2%">
-  <thead>
-    <tr>
-      <th>Title</th>
-      <th>Author</th>
-      <th>Views</th>
-    </tr>
-  </thead>
-  <tbody>
-  @foreach ($posts as $post)
-    <tr>
-      <td><a href="/posts/{{ rawurlencode($post->title) }}">{{ $post->title }}</a></td>
-      <td>{{ $post->author}}</td>
-      <td>{{ $post->view_count}}</td>
-    </tr>
-   @endforeach
-  </tbody>
-</table>
+@include('Partials._postsViewTable')
 
 @endsection
--- resources/views/Admin/userDashboard.blade.php
+++ resources/views/Admin/userDashboard.blade.php
@@ -6,6 +6,23 @@
 
 <a href="{{ route('admin.addAuthor') }}" class="btn btn-info" style="margin-top: 15px;"">Add Author</a>
 
-@include('Partials._postsViewTable')
+<table class="table table-hover" style="margin-top: 2%">
+  <thead>
+    <tr>
+      <th>First</th>
+      <th>Last</th>
+      <th>Action</th>
+    </tr>
+  </thead>
+  <tbody>
+  @foreach ($users as $user)
+    <tr>
+      <td>{{ $user->first_name}}</td>
+      <td>{{ $user->last_name}}</td>
+      <td><a href="{{ route('admin.editAuthor', $user->id) }}" class="btn btn-warning" style="margin-top: 15px;">Edit/Delete</a></td>
+    </tr>
+   @endforeach
+  </tbody>
+</table>
 
 @endsection
